adf4890ca3cffb01804c960ff90b729c5e94c520
wojthor <wojtek1aniszewski1@gmail.com>
2024-02-09 13:56:52 +0100

    little change for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ const products: ProductItemType[] = [
 export default function Home() {
 	return (
 		<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
-			<ProductList products={products} data-testid="products-list" />
+			<ProductList products={products} />
 		</section>
 	);
 }
